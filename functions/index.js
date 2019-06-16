644efda3c00379dc75d664961281546a7ccb5993
Unknown <rusditoriq101@gmail.com>
2019-06-16 23:59:20 +0700

    Setting Credential

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,15 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./service_account.json');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
 
-exports.goodBye = functions.https.onRequest((request,response)=>{
-    response.send('Good Bye');
-});
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://one-time-password-b5524.firebaseio.com"
+  });
 
 exports.createUser = functions.https.onRequest(createUser);
